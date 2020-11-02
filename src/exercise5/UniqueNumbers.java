9055e841e5264d3c365a175d84d81746d9440c52
AlexBoykov01 <aleksandar.boykov@fdiba.tu-sofia.bg>
2020-11-02 21:01:48 +0000

    .

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -4,23 +4,42 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class UniqueNumbers {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter number count: ");
-        int numberCount = input.nextInt();
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        int[] numbers = new int[numberCount];
+    System.out.print("Enter number count: ");
+    int numberCount = input.nextInt();
 
-        System.out.println("Enter numbers: ");
-        for (int index = 0; index < numbers.length; index++) {
-            numbers[index] = input.nextInt();
-        }
+    int[] numbers = new int[numberCount];
 
-        int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
-
-        String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
-        System.out.println("Unique numbers: " + uniqueNumbersAsString);
+    System.out.print("Enter numbers: ");
+    for (int index = 0; index < numbers.length; index++) {
+      numbers[index] = input.nextInt();
     }
+
+    int[] uniqueNumbers = new int[numbers.length];
+    for (int index = 0; index < numbers.length; index++) 
+    {
+      int a = numbers[index];
+      for (int i = index + 1; i < numbers.length - index; i++) 
+      {
+        if (a == numbers[i] && numbers[i] != 0) 
+        {
+          numbers[i] = 0;
+        }
+      }
+    }
+    int counter = 0;
+    for (int index = 0; index < numbers.length; index++) {
+      if (numbers[index] != 0) 
+      {
+        uniqueNumbers[counter] = numbers[index];
+        counter++;
+      }
+
+    }
+    String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
+    System.out.println("Unique numbers: " + uniqueNumbersAsString);
+  }
 }
